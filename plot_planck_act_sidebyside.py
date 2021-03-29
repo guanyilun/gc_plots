3dd4ea5d21fb5879e3d5bdc10959dd4a094f080d
Yilun Guan <zoom.aaron@gmail.com>
2021-03-29 10:36:02 -0400

    add axes and labels to planck act sidebyside plot

--- plot_planck_act_sidebyside.py
+++ plot_planck_act_sidebyside.py
@@ -1,8 +1,19 @@
-"""This script plots planck and act temperature maps side by side as a 3x2 grid"""
+"""This script plots planck and act temperature maps side by side as a 3x2 grid
+
+2021 Mar 26
+- add colorbar and axis coordinates
+"""
 
 import argparse, os, os.path as op
 import numpy as np, glob
 import matplotlib as mpl
+mpl.rcParams['xtick.direction']='in'
+mpl.rcParams['ytick.direction']='in'
+mpl.rcParams["xtick.major.size"]=2
+mpl.rcParams["ytick.major.size"]=2
+mpl.rcParams["xtick.minor.size"]=1
+mpl.rcParams["ytick.minor.size"]=1
+
 import matplotlib.pyplot as plt
 from matplotlib import colors
 from pixell import enmap, colorize
@@ -15,61 +26,60 @@ from common import *
 def process_map(imap):
     return np.log10(imap[0])
 
-def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
-    """build a colormap by truncating a known colormap"""
-    new_cmap = colors.LinearSegmentedColormap.from_list(
-        'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
-        cmap(np.linspace(minval, maxval, n)))
-    return new_cmap
-
 parser = argparse.ArgumentParser()
 parser.add_argument("-o","--odir", default='plots')
 parser.add_argument("--oname", default="test.pdf")
 parser.add_argument("--use", default='coadd')
+parser.add_argument("--area", default='half')
 args = parser.parse_args()
 if not op.exists(args.odir): os.makedirs(args.odir)
 
 # Build figure
-fig, axes = plt.subplots(3, 2, figsize=(9,7))
-box = np.array([[-1,2],[1,-2]]) / 180*np.pi
-
-planck_half = truncate_colormap(plt.get_cmap('planck'), minval=0.5)
+fig, axes = plt.subplots(3, 2, figsize=(9,6.5), gridspec_kw={'width_ratios':[0.93,1]})
+box = boxes[args.area]
 
 plot_opts = {
     'origin': 'lower',
-    'cmap': planck_half,
-    'vmin': 8.5,
-    'vmax': 10.5
+    'cmap': 'planck_half',
+    'extent': box2extent(box)/np.pi*180
 }
-# row 0: f090
-imap = load_map(filedb['f090']['planck'], box, fcode='f090')
-imap = process_map(imap)
-axes[0,0].imshow(imap, **plot_opts)
-imap = load_map(filedb['f090'][args.use], box, fcode='f090')
-imap = process_map(imap)
-axes[0,1].imshow(imap, **plot_opts)
 
-# row 1: f150
-imap = load_map(filedb['f150']['planck'], box, fcode='f150')
-imap = process_map(imap)
-axes[1,0].imshow(imap, **plot_opts)
-imap = load_map(filedb['f150'][args.use], box, fcode='f150')
-imap = process_map(imap)
-axes[1,1].imshow(imap, **plot_opts)
+for i, freq in zip(range(3), ['f090','f150','f220']):
+    # plotstyle
+    if   i == 0: norm = colors.LogNorm(vmin=10**-0.5, vmax=10**1.5)
+    elif i == 1: norm = colors.LogNorm(vmin=10**-0.5, vmax=10**1.5)
+    else:        norm = colors.LogNorm(vmin=10**0.5,  vmax=10**2)
+    # load data
+    imap = load_map(filedb[freq]['planck'], box, fcode=freq)/1e9
+    im_p = axes[i,0].imshow(imap[0], norm=norm, **plot_opts)
+    imap = load_map(filedb[freq][args.use], box, fcode=freq)/1e9
+    im_c = axes[i,1].imshow(imap[0], norm=norm, **plot_opts)
+    cb   = plotstyle.add_colorbar(fig, axes[i,1])
+    # only add colorbar label to the middle panel
+    if i == 1: fig.colorbar(im_c, cax=cb).set_label('Total Intensity [MJy/sr]')
+    else: fig.colorbar(im_c, cax=cb)
 
-# row 2: f220
-plot_opts.update({'vmin':9.5, 'vmax': 11})
-imap = load_map(filedb['f220']['planck'], box, fcode='f220')
-imap = process_map(imap)
-axes[2,0].imshow(imap, **plot_opts)
-imap = load_map(filedb['f220'][args.use], box, fcode='f220')
-imap = process_map(imap)
-axes[2,1].imshow(imap, **plot_opts)
+for i in range(3):  # row
+    for j in range(2):  # col
+        ax = axes[i,j]
+        if i == 2 and j == 0: pass
+        else: 
+            ax.axes.yaxis.set_ticklabels([])
+            ax.axes.xaxis.set_ticklabels([])
+        if j == 0:
+            ax.yaxis.set_ticks_position('left')
+            # ax.spines['right'].set_visible(False)
+        if j == 1:
+            ax.yaxis.set_ticks_position('right')
+            # ax.spines['left'].set_visible(False)
+        # if i != 2:
+            # ax.yaxis.set_ticks_position('right')
 
-# turn-off axis
-for i in range(3):
-    for j in range(2):
-        axes[i,j].axis('off')
+axes[1,0].axes.yaxis.set_ticklabels([])
+axes[-1,1].axes.xaxis.set_ticklabels([])
+
+axes[-1,0].set_xlabel('l [deg]')
+axes[-1,0].set_ylabel('b [deg]')
 
 # setup labels: Planck, ACT+Planck
 axes[0,0].text(0.4, 1.05, 'Planck',
@@ -80,16 +90,24 @@ axes[0,1].text(0.35, 1.05, 'ACT+Planck',
                transform=axes[0,1].transAxes, fontsize=12)
 
 # setup labels: f090, f150, f220
-axes[0,0].text(-0.05, 0.40, 'f090', rotation=90,
-               verticalalignment='bottom', horizontalalignment='left',
-               transform=axes[0,0].transAxes, fontsize=12)
-axes[1,0].text(-0.05, 0.40, 'f150', rotation=90,
-               verticalalignment='bottom', horizontalalignment='left',
-               transform=axes[1,0].transAxes, fontsize=12)
-axes[2,0].text(-0.05, 0.40, 'f220', rotation=90,
-               verticalalignment='bottom', horizontalalignment='left',
-               transform=axes[2,0].transAxes, fontsize=12)
-plt.tight_layout(w_pad=0.2, h_pad=0.2)
+
+# axes[0,0].text(-0.12, 0.40, 'f090', rotation=90,
+#                verticalalignment='bottom', horizontalalignment='left',
+#                transform=axes[0,0].transAxes, fontsize=12)
+# axes[1,0].text(-0.12, 0.40, 'f150', rotation=90,
+#                verticalalignment='bottom', horizontalalignment='left',
+#                transform=axes[1,0].transAxes, fontsize=12)
+# axes[2,0].text(-0.12, 0.40, 'f220', rotation=90,
+#                verticalalignment='bottom', horizontalalignment='left',
+#                transform=axes[2,0].transAxes, fontsize=12)
+plt.tight_layout(w_pad=-0.3, h_pad=-0.3)
+props = dict(alpha=1, facecolor='white')
+plt.text(0.47, 0.9, 'f090', transform=fig.transFigure, fontsize=12,
+         usetex=True, horizontalalignment='left', bbox=props)
+plt.text(0.47, 0.61, 'f150', transform=fig.transFigure, fontsize=12,
+         usetex=True, horizontalalignment='left', bbox=props)
+plt.text(0.47, 0.32, 'f220', transform=fig.transFigure, fontsize=12,
+         usetex=True, horizontalalignment='left', bbox=props)
 ofile = op.join(args.odir, args.oname)
 print("Writing:", ofile)
 plt.savefig(ofile, bbox_inches='tight')
